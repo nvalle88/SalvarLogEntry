5d7b471fb3f1ef4b6f49a28a595d7bcc935281e0
Néstor Yasmani Valle Rabelo <nestorvalle880416@gmail.com>
2017-08-14 11:45:20 -0500

    Se agregaron los enumeradores

---
+++ bd.guardarlog.servicios/Enumeradores/Enumeradores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bd.log.guardar.Enumeradores
+{
+    public enum LogCategoryParameter
+    {
+        Permission, Activation, Critical, ChangeInformation, NetActivity, Create, Edit, Delete
+    }
+
+    public enum LogLevelParameter
+    {
+        ERR, ADV, INFO
+    }
+}

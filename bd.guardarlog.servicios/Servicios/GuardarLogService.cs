b8caafcdff3279ad9b011f1261b8d5d4a3a8e4b0
Néstor Valle <nestorvalle880416@gmail.com>
2017-11-10 20:46:36 -0600

    modificación para soportar subsitios

--- bd.guardarlog.servicios/Servicios/GuardarLogService.cs
+++ bd.guardarlog.servicios/Servicios/GuardarLogService.cs
@@ -22,9 +22,10 @@ namespace bd.log.guardar.Servicios
                     var request = JsonConvert.SerializeObject(logEntryTranfer);
                     var content = new StringContent(request, Encoding.UTF8, "application/json");
 
-                    cliente.BaseAddress = new Uri(AppGuardarLog.BaseAddress);
+                   
                     var url = "/api/LogEntries/InsertarLonEntry";
-                    var respuesta = await cliente.PostAsync(url, content);
+                    var uri = string.Format("{0}/{1}", AppGuardarLog.BaseAddress, url);
+                    var respuesta = await cliente.PostAsync(new Uri(uri), content);
 
                     var resultado = await respuesta.Content.ReadAsStringAsync();
                     var response = JsonConvert.DeserializeObject<Response>(resultado);

8854461c809b3bce429cd4bbab9d24ba50e56521
Néstor Valle <nestorvalle880416@gmail.com>
2017-12-07 20:54:10 -0500

    Agregando funcionalidad para salvar log por ApiServicio

---
+++ bd.guardarlog.servicios/Utiles/EntradaLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bd.log.guardar.Utiles
+{
+   public class EntradaLog
+    {
+        public string ExceptionTrace { get; set; }
+        public string LogCategoryParametre { get; set; }
+        public string LogLevelShortName { get; set; }
+        public string ObjectPrevious { get; set; }
+        public string ObjectNext { get; set; }
+
+    }
+}
